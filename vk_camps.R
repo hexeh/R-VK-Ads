06d6cd19026f2c714ac786a8f2fde4ce559b38a3
hexeh <const.stuff@gmail.com>
2016-11-21 15:35:22 +0300

    Get a list of campaigns

---
+++ vk_camps.R
@@ -0,0 +1,20 @@
+campaignsVK <- function(vk_token, acc_id, cli_id, archive = 1,...)
+{
+  if("lib" %in% names(dots)){
+    library("httr", lib.loc = dots[["lib"]])
+  }
+  else
+  {
+    library("httr")
+  }
+  vk_camp <- GET("https://api.vk.com/method/ads.getCampaigns", query = list(
+    access_token = vk_token,
+    account_id = acc_id,
+    client_id = cli_id,
+    include_deleted = archive
+  ))
+  camp_list <- content(vk_camp)
+  vk_digest <- data.frame(do.call(rbind, camp_list$response))
+  vk_digest <- unique(vk_digest)
+  return(camp_list)
+}
